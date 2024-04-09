1ec12ec9a7ea4a68d339451811dc6da1c832fea9
MrRedhu <aaryanredhu@gmail.com>
2024-04-09 14:17:35 -0700

    Initial commit

---
+++ src/main/java/office/project360/HelloController.java
@@ -0,0 +1,14 @@
+package office.project360;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
